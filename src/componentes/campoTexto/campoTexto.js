cfd85e981245684cb3c9ad4c339108e00ddf3d22
Jonata Teixeira <67440063+chefejonata@users.noreply.github.com>
2023-03-14 14:09:14 -0300

    Add files via upload

---
+++ src/componentes/campoTexto/campoTexto.js
@@ -0,0 +1,22 @@
+import './campoTexto.css'
+
+const CampoTexto = (props) => {
+
+    const placeholderModificado = `${props.placeholder}...`
+
+
+
+    const aoDigitado = (evento) => {
+            props.aoAlterado(evento.target.value)
+   
+    }
+
+    return (
+        <div className='campo-texto'>
+            <label>{props.label}</label>
+            <input value={props.value} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificado}/>
+        </div>
+    )
+}
+
+export default CampoTexto
---
+++ src/componentes/formulario/Formulario.js
@@ -0,0 +1,80 @@
+import  { useState } from 'react' 
+import Botao from '../Botao/Botao'
+import CampoTexto from '../campoTexto/campoTexto'
+import ListaSuspensa from '../ListaSuspensa/listaSuspensa'
+import './Formulario.css'
+
+
+const Formulario = (props) => {
+
+const times = [
+    'Programação',
+    'Font-and',
+    'Data Science',
+    'Devops',
+    'UX e Desig',
+    'Mobile',
+    'Inovação e gestão'
+]
+
+const [nome, setNome] = useState('')
+const [cargo, setCargo] = useState('')
+const [imagem, setImagem] = useState('')
+const [time, setTime] = useState('')
+
+const aoSalvar = (evento) => {
+    evento.preventDefault()
+    props.aoColaboradorCadastrado({
+        nome,
+        cargo,
+        imagem,
+        time
+    })
+    setNome('')
+    setCargo('')
+    setImagem('')
+    setTime('')
+}
+
+    return (
+        <section className='formulario'>
+            <form onSubmit={aoSalvar}>
+            <h2>Preencha os cardes com informações do colaborador</h2>
+            <CampoTexto
+             obrigatorio={true} 
+             label="Nome" 
+             placeholder='Digite seu nome' 
+             valor={nome}
+             aoAlterado = {valor => setNome(valor)}
+             />
+
+            <CampoTexto 
+            obrigatorio={true} 
+            label="Cargo" 
+            placeholder='Digite seu cargo' 
+            valor={cargo}
+            aoAlterado ={valor => setCargo(valor)}
+            />
+            <CampoTexto 
+            label="Imagem"  
+            placeholder='Digite o endereço da imagem'
+            valor={imagem}
+            aoAlterado ={valor => setImagem(valor)}
+            />
+            <ListaSuspensa 
+            obrigatorio={true} 
+            label='Time' 
+            itens={props.times}
+            valor={time}
+            aoAlterado ={valor => setTime(valor)} 
+            
+            />
+            <Botao>
+                Criar Card
+            </Botao>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario
